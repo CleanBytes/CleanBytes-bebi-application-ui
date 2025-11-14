e2aadffa6995beec9add705ce4224a472c9414aa
Mattéo Gauthy <CleanBytes>
2025-11-14 12:08:56 +0100

    Update SensorStatCard global style and responsive layout

--- src/components/organisms/SensorStatCard.tsx
+++ src/components/organisms/SensorStatCard.tsx
@@ -1,34 +1,67 @@
 import type React from "react";
-import CircularProgressBar from "../molecules/CircularProgressBar";
+import CloudMoodProgressBar from "./CloudMoodProgressBar";
 
 
 interface SensorStatCardProps {
     title: string;
-    value: number;
-    max: number;
-    unit: string;
     description: string;
+    max: number;
+    value: number;
+    optimalValueMin: number;
+    optimalValueMax: number;
+    unit: string;
+
 }
 
-const SensorStatCard : React.FC<SensorStatCardProps> = ({ title, value, max, unit, description }) => {
+const SensorStatCard : React.FC<SensorStatCardProps> = ({ 
+    title, 
+    description,
+    max,
+    value, 
+    unit, 
+    optimalValueMin, 
+    optimalValueMax, 
+    
+     }) => {
     return (<>
-        <div className="w-full max-w-xl bg-slate-900/30 px-5 py-7 rounded-3xl">
-            <div className="flex justify-between items-center">
-                <div className="font-[Dynapuff] flex flex-col gap-y-3">
-                    <h2 className="text-white text-3xl font-extrabold">{ title }</h2>
+
+        <div className="w-full bg-zinc-950/20 p-4 sm:p-6 lg:p-8 rounded-3xl">
+            <div className="font-[Dynapuff] flex justify-between items-start gap-x-4">
+                <div className="flex flex-col gap-y-3">
+                    <h2 className="text-white text-xl sm:text-3xl lg:text-4xl font-extrabold">
+                        { title }
+                    </h2>
                     <div className="font-[Knewave] text-white flex items-end gap-x-2 ">
-                        <span className="text-6xl text-zinc-200 font-extrabold">{ value }</span>
-                        <span className="text-5xl font-semibold">{ unit }</span>
+                        <span className="text-4xl sm:text-6xl text-zinc-200 font-extrabold">
+                            { value }
+                        </span>
+                        <span className="text-2xl sm:text-4xl font-semibold opacity-80">
+                            { unit }
+                        </span>
                     </div>
-                    <span className="text-gray-300 text-lg font-light">{ description}</span>
+                    <span className="text-gray-300 text-sm sm:text-xl lg:text-base font-light">{ description}</span>
                 </div>
 
-                <div>
-                    <CircularProgressBar
-                        size={190}
-                        strokeWidth={32}
-                        outerStrokeColor="white"
-                        innerStrokeColor="#ebcad5"
+                <div className="sm:hidden">
+                    <CloudMoodProgressBar
+                        size={100}
+                        strokeWidth={12}
+                        outerStrokeColor="#f0e8f0"
+                        innerStrokeColor="#ffadcd"
+                        optimalValueMin={optimalValueMin}
+                        optimalValueMax={optimalValueMax}
+                        value={value}
+                        max={max}
+                    /> 
+                </div>
+                <div className="hidden sm:inline">
+                    <CloudMoodProgressBar
+                        size={160}
+                        strokeWidth={16}
+                        outerStrokeColor="#f0e8f0"
+                        innerStrokeColor="#ffadcd"
+                        optimalValueMin={optimalValueMin}
+                        optimalValueMax={optimalValueMax}
                         value={value}
                         max={max}
                     /> 
